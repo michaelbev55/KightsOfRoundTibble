edda1c32aa6692212c505636bb3ce829fdbd5b5f
Andrew Gans <andrewgans@andrews-mbp-75.bc.edu>
2023-09-13 17:47:03 -0400

    Work up until customzied functions -AG

--- KnightsOfRoundTibble_module2.R
+++ KnightsOfRoundTibble_module2.R
@@ -12,3 +12,14 @@ pseed2 <- pseed%>%
 pseed2 <- pseed2%>%
   left_join(pseed.bl,by="fish")%>%
   print()
+#Computer specific speed by speed/ body length for each fish 
+pseed2 <- pseed2%>%
+  mutate(bl.s=cm.s/bl)%>%
+  print()
+#Plot fin amplitudes over specific speeds
+pseed2%>%
+  ggplot(aes(x=bl.s,y=amp.bl))+geom_point(alpha=0.01)
+#Left fin amplitude for a specific date
+pseed2%>%
+  filter(date=="2019-06-17-151149", fin=="L")%>%
+  ggplot(aes(x=frame,y=amp.bl))+geom_point()
